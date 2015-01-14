4342e8067304cd5555ffd8c4338236e055a7f53d
hmlingesh <Lingesh.M@happiestminds.com>
2015-01-14 21:51:14 +0530

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -38,10 +38,10 @@ $APP_ROOT/apache2/bin/httpd -k start -f $APP_ROOT/apache2/conf/httpd.conf
 
 #exec $APP_ROOT/apache-tomcat-7.0.57/bin/startup.sh
 
-exec JAVA_HOME=$APP_ROOT/java JAVA_OPTS=\"-Djava.io.tmpdir=$TMPDIR
- -XX:OnOutOfMemoryError=$APP_ROOT/java/bin/killjava.sh -Xmx382293K
- -Xms382293K -XX:PermSize=64M -Xss995K -Dhttp.port=$PORT\"
- $APP_ROOT/apache-tomcat-7.0.57/bin/catalina.sh run
+#exec JAVA_HOME=$APP_ROOT/java JAVA_OPTS=\"-Djava.io.tmpdir=$TMPDIR
+ #-XX:OnOutOfMemoryError=$APP_ROOT/java/bin/killjava.sh -Xmx382293K
+ #-Xms382293K -XX:PermSize=64M -Xss995K -Dhttp.port=$PORT\"
+ #$APP_ROOT/apache-tomcat-7.0.57/bin/catalina.sh run
 echo "Apache has been started.... $(netstat -a)"
 
 #sleep 1m
