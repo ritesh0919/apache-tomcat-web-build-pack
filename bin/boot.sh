b1773d3be20f66e5795a24659b178e4678d2394c
hmlingesh <Lingesh.M@happiestminds.com>
2015-01-14 22:44:50 +0530

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -38,8 +38,8 @@ $APP_ROOT/apache2/bin/httpd -k start -f $APP_ROOT/apache2/conf/httpd.conf
 
 #exec $APP_ROOT/apache-tomcat-7.0.57/bin/startup.sh
 
-exec JAVA_HOME=$APP_ROOT/jdk1.8.0_25 JAVA_OPTS=\"-Djava.io.tmpdir=$TMPDIR
-  -Dhttp.port=$PORT\" $APP_ROOT/apache-tomcat-7.0.57/bin/catalina.sh run
+#exec JAVA_HOME=$APP_ROOT/jdk1.8.0_25 JAVA_OPTS=\"-Djava.io.tmpdir=$TMPDIR
+ # -Dhttp.port=$PORT\" $APP_ROOT/apache-tomcat-7.0.57/bin/catalina.sh run
 echo "Apache has been started.... $(netstat -a)"
 
 #sleep 1m
