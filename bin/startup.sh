d5d7b4f0ab851534063a197e36822060dde92eaf
hmlingesh <Lingesh.M@happiestminds.com>
2015-01-19 17:57:54 +0530

    Update startup.sh

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,6 +1,6 @@
 # ------------------------------------------------------------------------------------------------
 #JAVA_HOME=$HOME/jdk1.8.0_25 JAVA_OPTS="-Djava.io.tmpdir=$TMPDIR -Dhttp.port=$VCAP_APP_PORT" $HOME/apache-tomcat-7.0.57/bin/catalina.sh run
-JAVA_HOME=$HOME/jdk1.8.0_25 JAVA_OPTS="-Djava.io.tmpdir=$TMPDIR" $HOME/apache-tomcat-7.0.57/bin/catalina.sh run
+JAVA_HOME=$HOME/jdk1.8.0_25 JAVA_OPTS="-Djava.io.tmpdir=$TMPDIR -Dhttp.port=8080" $HOME/apache-tomcat-7.0.57/bin/catalina.sh run
 echo "Tomcat has been started.... $(netstat -tulpen)"
 
 # ------------------------------------------------------------------------------------------------
