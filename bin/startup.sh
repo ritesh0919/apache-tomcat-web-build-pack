979354a5935aa8ba861950297753940057a907df
hmlingesh <Lingesh.M@happiestminds.com>
2015-01-20 11:49:27 +0530

    Update startup.sh

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,8 +1,8 @@
 # ------------------------------------------------------------------------------------------------
 #JAVA_HOME=$HOME/jdk1.8.0_25 JAVA_OPTS="-Djava.io.tmpdir=$TMPDIR -Dhttp.port=$VCAP_APP_PORT" $HOME/apache-tomcat-7.0.57/bin/catalina.sh run
-JAVA_HOME=$HOME/jdk1.8.0_25 JAVA_OPTS="-Djava.io.tmpdir=$TMPDIR -Dhttp.port=8080" $HOME/apache-tomcat-7.0.57/bin/catalina.sh run
+JAVA_HOME=$HOME/jdk1.8.0_25 JAVA_OPTS="-Djava.io.tmpdir=$TMPDIR -Dhttp.port=55555" $HOME/apache-tomcat-7.0.57/bin/catalina.sh run
 
-until [ "`curl --silent --show-error --connect-timeout 1 -I http://localhost:8080 | grep 'It works'`" != "" ];
+until [ "`curl --silent --show-error --connect-timeout 1 -I http://localhost:55555 | grep 'It works'`" != "" ];
 do
   sleep 5
 done
