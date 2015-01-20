c10dedb571d1864b6af7a1db0a72e3b1d7ffcf86
hmlingesh <Lingesh.M@happiestminds.com>
2015-01-20 14:57:32 +0530

    Update startup.sh

--- bin/startup.sh
+++ bin/startup.sh
@@ -2,6 +2,10 @@
 #JAVA_HOME=$HOME/jdk1.8.0_25 JAVA_OPTS="-Djava.io.tmpdir=$TMPDIR -Dhttp.port=$VCAP_APP_PORT" $HOME/apache-tomcat-7.0.57/bin/catalina.sh run
 JAVA_HOME=$HOME/jdk1.8.0_25 JAVA_OPTS="-Djava.io.tmpdir=$TMPDIR -Dhttp.port=55555" $HOME/apache-tomcat-7.0.57/bin/catalina.sh run
 
+echo **************************************
+wget http://localhost:55555/examples
+echo **************************************
+
 until [ "`curl --silent --show-error --connect-timeout 1 -I http://localhost:55555 | grep 'It works'`" != "" ];
 do
   sleep 5
