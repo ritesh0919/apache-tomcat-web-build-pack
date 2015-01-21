d82931794fac3500386e0c5ca36c7acf83d2d33c
hmlingesh <Lingesh.M@happiestminds.com>
2015-01-21 16:43:47 +0530

    Update startup.sh

--- bin/startup.sh
+++ bin/startup.sh
@@ -4,7 +4,7 @@ cd $HOME/jdk1.8.0_25
 echo "----> list of files and directory:$(ls -l)" 
 # ------------------------------------------------------------------------------------------------
 #JAVA_HOME=$HOME/jdk1.8.0_25 JAVA_OPTS="-Djava.io.tmpdir=$TMPDIR -Dhttp.port=$VCAP_APP_PORT" $HOME/apache-tomcat-7.0.57/bin/catalina.sh run
-JAVA_HOME=$HOME/jdk1.8.0_25/jdk1.8.0_25 JAVA_OPTS="-Djava.io.tmpdir=$TMPDIR -Dhttp.port=55555" $HOME/apache-tomcat-7.0.57/bin/catalina.sh run
+JAVA_HOME=$HOME/jdk1.8.0_25/jdk1.8.0_25 JAVA_OPTS="-Djava.io.tmpdir=$TMPDIR -Dhttp.port=77777" $HOME/apache-tomcat-7.0.57/bin/catalina.sh run
 
 
 
@@ -16,5 +16,5 @@ JAVA_HOME=$HOME/jdk1.8.0_25/jdk1.8.0_25 JAVA_OPTS="-Djava.io.tmpdir=$TMPDIR -Dht
 
 # ------------------------------------------------------------------------------------------------
 echo **************************************
-wget http://localhost:55555/examples
+wget http://localhost:77777/examples
 echo **************************************
